34a5cb6f7416f3bd0d8c0d8456ecde622649c7f2
MCPolichette <mark@polichette.com>
2019-02-09 13:41:59 -0700

    making progress, learning reactnative syntax

--- countdown/App.js
+++ countdown/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import EventList from './EventList';
 
 export default class App extends React.Component {
   render() {
+
     return (
-      <View style={styles.container}>
-        <Text>HELLO World!</Text>
-        <Text>Welcome to My First App!</Text>
-      </View>
+      <EventList />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
