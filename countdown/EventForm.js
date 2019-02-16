e962e4b6b2532bd49cf222898302c3f8d9af5b7d
MCPolichette <mark@polichette.com>
2019-02-16 10:46:40 -0700

    navigation fixed

--- countdown/EventForm.js
+++ countdown/EventForm.js
@@ -5,3 +5,25 @@ import {
     TouchableHighlight,
 
 } from 'react-native'
+
+class EventForm extends Component {
+    handleAddPress = () => {
+        // ?
+    }
+
+
+
+    render() {
+        return (
+            <View>
+                <TouchableHighlight
+                    onPress={this.handleAddPress}
+                >
+
+                    <Text>Add</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
+export default EventForm
